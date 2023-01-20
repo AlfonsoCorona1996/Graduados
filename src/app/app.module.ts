53fdb15538a2d739b2c81c50d0b900adc9b4a68a
AlfonsoCorona1996 <alfonso.corona1996@gmail.com>
2023-01-20 13:49:54 -0600

    full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InicioComponent } from './inicio/inicio.component';
+import { PortadaComponent } from './portada/portada.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    PortadaComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
+PortadaComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
